987f3db74e5e008093007bd73bb90140747217f7
Donald Lobree <edl1155@gmail.com>
2019-11-30 21:30:12 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do | color_gender_lives, value |
+    #color_gender_lives variable holds the 3 symbol keys :color, :gender, :lives
+    value.each do | stats, all_names |
+      #stats are all the keys and strings rep qualities of the birds
+      all_names.each do | name |
+        if pigeon_list[name] == nil
+          pigeon_list[name] = {}
+        end
+        if pigeon_list[name][color_gender_lives] == nil
+          pigeon_list[name][color_gender_lives] = []
+        end
+        pigeon_list[name][color_gender_lives].push(stats.to_s)
+      end
+
+    end
+  end
+  pigeon_list
+
 end
