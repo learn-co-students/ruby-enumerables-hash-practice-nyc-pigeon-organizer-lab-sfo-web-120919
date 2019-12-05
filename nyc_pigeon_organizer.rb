6d602ff93b5629713cb39c3c3904dc9574f0ce99
Nick Corona <coronaisme@gmail.com>
2019-12-05 20:49:56 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  updated_data = {}
+  
+  data.each do |key, value|
+    value.each do |new_value, names|
+      names.each do |name|
+        
+        unless updated_data[name]
+          updated_data[name] = {}
+        end
+        
+        unless updated_data[name][key]
+          updated_data[name][key] = []
+        end
+        
+        updated_data[name][key].push(new_value.to_s)
+      end
+    end
+  end
+  updated_data
 end
